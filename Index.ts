b36c6dd38cf5e00dee517d366a2bda73cbfaab74
reema29 <reemasiddiqui2995@gmail.com>
2024-04-20 03:03:58 +0500

    first commit

---
+++ Index.ts
@@ -0,0 +1,37 @@
+import inquirer from "inquirer" ;
+import chalk from "chalk" ;
+
+
+
+// Asking Questions from Users through Inquirer
+
+let answers = await inquirer.prompt([
+    {message: "Enter your first number", type: "number", name: "firstNumber"},
+    {message: "Enter your second number", type: "number", name: "secondNumber"},
+    {
+        message: "Select One Operator to perform Operations",
+        type: "list", 
+        name: "operator",
+        choices: ["Addition","Subtraction","Multipication","Division"],
+    },
+]);
+
+//Conditional statements If-Else
+
+if(answers.operator === "Addition"){
+    console.log(answers.firstNumber + answers.secondNumber)
+}
+else if (answers.operator === "Subtraction"){
+     console.log(answers.firstNumber - answers.secondNumber)
+}
+else if (answers.operator === "Multipication"){
+    console.log(answers.firstNumber * answers.secondNumber)
+}
+else if (answers.operator === "Division"){
+    console.log(answers.firstNumber / answers.secondNumber)
+}
+else{
+    console.log("Invalid Input")
+}
+
+console.log("The End");
